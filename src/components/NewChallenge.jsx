bd4a0087b47feaec49ee21ea99852304431a6e16
Wojciech Galecki <wojciech.galecki@wp.pl>
2025-01-10 14:37:30 +0100

    layout changes animation

--- src/components/NewChallenge.jsx
+++ src/components/NewChallenge.jsx
@@ -76,7 +76,7 @@ export default function NewChallenge({ onDone }) {
                 hidden: { opacity: 0, scale: 0.5 },
                 visible: { opacity: 1, scale: [0.8, 1.3, 1] },
               }}
-              exit={{ opacity: 1, scale: 1 }} // override from a parent 'Modal' animation state
+              //exit={{ opacity: 1, scale: 1 }} // override from a parent 'Modal' animation state
               transition={{ type: "spring" }}
               key={image.alt}
               onClick={() => handleSelectImage(image)}
